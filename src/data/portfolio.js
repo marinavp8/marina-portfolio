efe97a00bbda7542b8f6529e9ca9ff65fe70031a
marinavp8 <100945399+marinavp8@users.noreply.github.com>
2023-08-16 10:54:49 +0200

    changeindex

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [{
 
     title: 'Marketing Strategy',
-    imgUrls: "/Captura de Pantalla 2023-03-01 a las 12.23.24.png",
+    imgUrls: "public/Captura de Pantalla 2023-03-01 a las 12.23.24.png",
     stack: ['HTML', 'CSS', 'JavaScript'],
     link: "https://fkcodes.com/",
 },
 {
     title: 'Developer Portfolio',
-    imgUrls: '/DSC_2477.JPG',
+    imgUrls: 'public/DSC_2477.JPG',
     stack: ['React', 'TailwindCSS', 'Vite'],
     link: 'https://thehackershideout.com/',
 },
 {
     title: 'AI Nerf Gun',
-    imgUrls: '/carbon.png',
+    imgUrls: 'public/carbon.png',
     stack: ['Python', 'C++', 'Robotics'],
     link: 'https://youtu.be/A8eegxL82PI',
 },
