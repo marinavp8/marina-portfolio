add649b161357184e83f5374e9c76493ef43cc64
marinavp8 <100945399+marinavp8@users.noreply.github.com>
2023-08-19 11:47:11 +0200

    final-changes

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -5,7 +5,7 @@ function TimelineItem({ year, title, company, details }) {
         <ol className="flex flex-col md-flex-row relative border-l border-stone-500">
             <li className="mb-10 ml-4">
                 <div className=" absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border- border-white" />
-                <p className=" flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
+                <div className=" flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
                     <span className="inline-block px-2 py-1 font-semibold text-white dark:text-stone-900 bg-stone-900 dark:bg-white rounded-md">
                         {year}
                     </span>
@@ -15,7 +15,7 @@ function TimelineItem({ year, title, company, details }) {
                     <p className="my-1 text-sm font-normal leading-none text-stone-400 ">
                         {company}
                     </p>
-                </p>
+                </div>
                 <p className="my-2 text-base font-normal text-stone-500 dark:text-stone-300">
                     {details}
                 </p>
