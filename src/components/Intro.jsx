824f3c531a0aa1dedac2fdc26d68451e9b6d6a47
marinavp8 <100945399+marinavp8@users.noreply.github.com>
2023-09-04 20:05:48 +0200

    new intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,11 @@ function Intro() {
             <h1 className="text-4xl md:text-7x1 mb-1 md:mb-3 font-bold">Marina</h1>
             <p className=" text-base md:text-sl mb-3 font-medium">Web developer & Marketing strategist</p>
             <p className="text sm max-w-xs mb-6 font-bold text-justify">
-                Hola! Soy Marina, una creativa del siglo 21. Me embarqué en la fascinante travesía del marketing, me sumergí en las profundidades del diseño web, y ahora estoy navegando por las aguas tumultuosas del desarrollo web.
+            * Marketing Strategist: Proven ability to unearth opportunities in saturated markets.
+            * Innovative Mindset: Thrive on creating unique strategies to drive brand success.
+            *Web Developer in Process: Relentless curiosity and passion for flawless online functionality.
+            *Creativity Meets Code: A unique blend where strategy intertwines with every pixel.
+            *Continuous Learner: Always at the crossroads of marketing and web development, ensuring brands not only stand out but also perform seamlessly online.
                 <br /><br /><br />
                 ¿Quieres saber más?
                {" "}
