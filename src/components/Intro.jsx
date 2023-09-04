f147e8ed8906b8f8ad9174962bb2ef9066bcbc41
marinavp8 <100945399+marinavp8@users.noreply.github.com>
2023-09-04 20:07:00 +0200

    justify left

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,7 @@ function Intro() {
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
             <h1 className="text-4xl md:text-7x1 mb-1 md:mb-3 font-bold">Marina</h1>
             <p className=" text-base md:text-sl mb-3 font-medium">Web developer & Marketing strategist</p>
-            <p className="text sm max-w-xs mb-6 font-bold text-justify">
+            <p className="text sm max-w-xs mb-6 font-bold text-justify-left">
             * Marketing Strategist: Proven ability to unearth opportunities in saturated markets.
             * Innovative Mindset: Thrive on creating unique strategies to drive brand success.
             *Web Developer in Process: Relentless curiosity and passion for flawless online functionality.
