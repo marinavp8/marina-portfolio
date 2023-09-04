e42a96f865a68849c9690090e3dfa6c471005ea7
marinavp8 <100945399+marinavp8@users.noreply.github.com>
2023-09-04 20:11:06 +0200

    new intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,13 +4,17 @@ function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
             <h1 className="text-4xl md:text-7x1 mb-1 md:mb-3 font-bold">Marina</h1>
-            <p className=" text-base md:text-sl mb-3 font-medium">Web developer & Marketing strategist</p>
-            <p className="text sm max-w-xs mb-6 font-bold text-justify-left">
-            * Marketing Strategist: Proven ability to unearth opportunities in saturated markets.
-            * Innovative Mindset: Thrive on creating unique strategies to drive brand success.
-            *Web Developer in Process: Relentless curiosity and passion for flawless online functionality.
-            *Creativity Meets Code: A unique blend where strategy intertwines with every pixel.
-            *Continuous Learner: Always at the crossroads of marketing and web development, ensuring brands not only stand out but also perform seamlessly online.
+            <p className=" text-base md:text-sl mb-3 font-medium">When marketing meets code</p>
+            <p className="text sm max-w-xs mb-6 font-bold text-justify">
+            Marketing Maestro: Expertise in spotting market opportunities.
+            <br />
+            Innovation Driver: Elevating brands to new heights.
+            <br />
+            Web Dev Enthusiast: Merging design with functionality.
+            <br />
+            Data Analyst: Transforming data into actionable insights.
+            <br />
+            Lifelong Learner: At the nexus of marketing and tech.
                 <br /><br /><br />
                 ¿Quieres saber más?
                {" "}
